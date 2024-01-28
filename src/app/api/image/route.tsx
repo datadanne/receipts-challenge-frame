bc5d747d534e9e097b18a0bf55087c7d896d2680
Daniel Öhrlund <danielohrlund@gmail.com>
2024-01-28 15:15:46 +0100

    add frame implementation

---
+++ src/app/api/image/route.tsx
@@ -0,0 +1,95 @@
+import { NextResponse } from "next/server";
+import satori from "satori";
+import sharp from "sharp";
+import { join } from "path";
+import * as fs from "fs";
+
+const interPath = join(process.cwd(), "public/Inter-Regular.ttf");
+let inter = fs.readFileSync(interPath);
+
+interface Response {
+  data: {
+    total_miles: string;
+  };
+}
+
+export async function GET() {
+  const response = await fetch("https://leaderboard.receipts.xyz/api/eas", {
+    cache: "no-store",
+  });
+  const { data } = (await response.json()) as Response;
+
+  const now = new Date();
+  const deadline = new Date("2024-01-29T00:00:00Z");
+  const hoursLeft = Math.max(0, (deadline.getTime() - now.getTime()) / 36e5);
+
+  const svg = await satori(
+    <div
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "black",
+        color: "white",
+        textTransform: "uppercase",
+        fontSize: "14px",
+        borderRadius: "10px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingLeft: "15px",
+          paddingRight: "15px",
+          flexGrow: 1,
+        }}
+      >
+        <span>Challenge 001</span>
+        <span>Time remaining: {Math.ceil(hoursLeft)} hours</span>
+      </div>
+      <img src="https://leaderboard.receipts.xyz/1000_miles_challenge_banner.png" />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexGrow: 1,
+          alignItems: "center",
+        }}
+      >
+        <span>{data.total_miles} miles completed</span>
+      </div>
+    </div>,
+    {
+      width: 416,
+      height: 180.78,
+      fonts: [
+        {
+          name: "Inter",
+          data: inter,
+          weight: 400,
+          style: "normal",
+        },
+      ],
+    }
+  );
+
+  const img = await sharp(Buffer.from(svg))
+    .resize(1200)
+    .toFormat("png")
+    .toBuffer();
+
+  return new NextResponse(img, {
+    status: 200,
+    headers: {
+      "Content-Type": "image/png",
+      "Cache-Control": "max-age=60",
+    },
+  });
+}
